1354cff2f839e63aa094552ff81111440e81b664
Jesse Laning <1132336+jamolnng@users.noreply.github.com>
2022-05-30 15:10:19 -0400

    add helper scripts

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@ docker stop lder
 docker rm lder
 JEKYLL_ENV=production bundle exec jekyll b
 docker build -t lder .
-docker run --name lder -dp 3000:80 lder
+docker run -v $PWD/_site:/usr/share/nginx/html --name lder -dp 3000:80 lder
