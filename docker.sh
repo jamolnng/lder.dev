f6d2955d268e0268ccbad2e64e2a29bef406fd40
Jesse Laning <1132336+jamolnng@users.noreply.github.com>
2022-05-30 23:34:20 -0400

    update docker.sh helper

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 docker stop lder
 docker rm lder
-JEKYLL_ENV=production bundle exec jekyll b
+sh ./build.sh
 docker build -t lder .
-docker run -v $PWD/_site:/usr/share/nginx/html --name lder -dp 3000:80 lder
+docker run -v $PWD/_site:/usr/share/nginx/html --name lder -dp 80:80 lder
